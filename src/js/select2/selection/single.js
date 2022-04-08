4267d0e54be2bfea9d335bf8e11dce82ec7d912e
Abdulaziz Alshmasi <a2h.alshmasi@gmail.com>
2022-04-08 04:39:53 +0300

    Take aria-label and aria description attributes from initial select box by default. If one of them is not available: - Use the initial way for dealing with aria labeling, falling back to aria-labelledby instead, and using the IdRef of the original element as a label reference. - use the translatable messages for aria description.

--- src/js/select2/selection/single.js
+++ src/js/select2/selection/single.js
@@ -36,7 +36,8 @@ define([
       .attr('id', id)
       .attr('role', 'textbox')
       .attr('aria-readonly', 'true');
-    this.$selection.attr('aria-labelledby', id);
+    this.$element.attr('aria-label') ? this.$selection.attr('aria-label', this.$element.attr('aria-label')) : this.$selection.attr('aria-labelledby', id);
+	this.$element.attr('aria-description') ? this.$selection.attr('aria-description', this.$element.attr('aria-description')) : this.$selection.attr('aria-description', this.options.get('translations').get('selectBoxAriaDescription')());
     this.$selection.attr('aria-controls', id);
 
     this.$selection.on('mousedown', function (evt) {
